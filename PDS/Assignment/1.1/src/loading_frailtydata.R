1b7a0fdab21a95c99af66a5433d07382b27c16dd
ganeshkoneti01 <126365714+ganeshkoneti01@users.noreply.github.com>
2023-02-25 19:26:06 -0600

    Add files via upload

---
+++ PDS/Assignment/1.1/src/loading_frailtydata.R
@@ -0,0 +1,6 @@
+#Reading the csv file
+frailty = read.csv("C:\\Users\\ganes\\Documents\\Assignment\\1.1\\data_raw\\raw_frailty_data1.csv")
+frailty
+
+#Getting the info of complete csv
+summary(frailty)
---
+++ PDS/Assignment/1.2/src/loading_StudentsData.R
@@ -0,0 +1,6 @@
+#Reading the csv file
+Student = read.csv("C:\\Users\\ganes\\Documents\\Assignment\\1.2\\data_raw\\Raw_StudentsPerformance.csv")
+Student
+
+#Getting the info of complete csv
+summary(Student)
